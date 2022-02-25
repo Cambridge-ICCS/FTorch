ef6e9892a371636e6a4ef35b36c1609c22bb601c
Athena Elafrou <ae488@cam.ac.uk>
2022-02-25 15:07:32 +0000

    Cleanup of Fortran inference example

--- fortran-pytorch-lib/ts_inference.f90
+++ fortran-pytorch-lib/ts_inference.f90
@@ -1,57 +1,34 @@
 program inference
 
-   use, intrinsic :: iso_fortran_env, only: int32, int64, real32
-   use, intrinsic :: iso_c_binding, only: c_int, c_float, c_char, c_null_char, c_ptr, c_loc
+   use, intrinsic :: iso_c_binding, only: c_int64_t, c_float, c_char, c_null_char, c_ptr, c_loc
    use mod_torch
 
    implicit none
 
-   character(len=:), allocatable :: filename
-   integer(int32), parameter :: dims = 4
-   integer(int64) :: shape(dims) = [1, 3, 224, 224]
-   type(c_ptr) :: input_data
-   real(c_float), allocatable, target :: data(:)
-   type(torch_tensor) :: input, output
    type(torch_module) :: model
+   type(torch_tensor) :: input_tensor, output_tensor
+   real(c_float), allocatable, target :: input_data(:)
+   real(c_float), dimension(:), pointer :: output_data
+   type(c_ptr) :: input_data_ptr, output_data_ptr
+   integer(c_int), parameter :: input_dims = 4
+   integer(c_int64_t) :: input_shape(input_dims) = [1, 3, 224, 224]
+   character(len=:), allocatable :: filename
 
-   allocate(data(product(shape)))
-   data = 1.0d0
-   input_data = c_loc(data)
+   allocate(input_data(product(input_shape)))
+   input_data = 1.0d0
+   input_data_ptr = c_loc(input_data)
 
    ! Create input tensor
-   input = torch_tensor_from_blob(input_data, dims, shape, 6_c_int, 0_c_int)
+   input_tensor = torch_tensor_from_blob(input_data_ptr, input_dims, input_shape, torch_kFloat32, torch_kCPU)
    ! Load ML model
    model = torch_module_load(c_char_"../annotated_cpu.pt"//c_null_char)
    ! Deploy
-   output = torch_module_forward(model, input)
+   output_tensor = torch_module_forward(model, input_tensor)
 
    ! Cleanup
    call torch_module_delete(model)
-   call torch_tensor_delete(input)
-   call torch_tensor_delete(output)
-   deallocate(data)
-
-contains
-
-   subroutine alloc(a, n)
-      real, allocatable, intent(inout) :: a(:)
-      integer, intent(in) :: n
-      integer :: stat
-      character(100) :: errmsg
-
-      if (allocated(a)) call free(a)
-      allocate(a(n), stat=stat, errmsg=errmsg)
-      if (stat > 0) error stop errmsg
-   end subroutine alloc
-
-   subroutine free(a)
-      real, allocatable, intent(inout) :: a(:)
-      integer :: stat
-      character(100) :: errmsg
-
-      if (.not. allocated(a)) return
-      deallocate(a, stat=stat, errmsg=errmsg)
-      if (stat > 0) error stop errmsg
-   end subroutine free
+   call torch_tensor_delete(input_tensor)
+   call torch_tensor_delete(output_tensor)
+   deallocate(input_data)
 
 end program inference
