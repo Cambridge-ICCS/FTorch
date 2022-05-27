dd61d6c2b4ce91f5f8f9af8d2d8d40ebfbb34ccb
Athena Elafrou <ae488@cam.ac.uk>
2022-05-27 23:04:39 +0100

    Fix in build.sh of conda recipe

--- fortran-pytorch-lib/conda-recipe/build.sh
+++ fortran-pytorch-lib/conda-recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd fortran-pytorch-lib
+#cd fortran-pytorch-lib
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH=${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
