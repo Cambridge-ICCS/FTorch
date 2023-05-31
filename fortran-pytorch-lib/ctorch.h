1e6689103e3122080240f8c4af30787a78ab2cac
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-05-31 16:35:58 +0100

    Add doxygen info to ctorch header file.

--- fortran-pytorch-lib/ctorch.h
+++ fortran-pytorch-lib/ctorch.h
@@ -28,25 +28,92 @@ typedef enum {
 // Device types
 typedef enum { torch_kCPU, torch_kCUDA } torch_device_t;
 
+
+// =====================================================================================
 // Tensor API
+// =====================================================================================
+/**
+ * Function to generate a Torch Tensor of zeros
+ * @param number of dimensions of the Tensor
+ * @param shape of the Tensor
+ * @param data type of the elements of the Tensor
+ * @param device used (cpu, CUDA, etc.)
+ */
 EXPORT_C torch_tensor_t torch_zeros(int ndim, const int64_t* shape,
                                     torch_data_t dtype, torch_device_t device);
+
+/**
+ * Function to generate a Torch Tensor of ones
+ * @param number of dimensions of the Tensor
+ * @param shape of the Tensor
+ * @param data type of the elements of the Tensor
+ * @param device used (cpu, CUDA, etc.)
+ */
 EXPORT_C torch_tensor_t torch_ones(int ndim, const int64_t* shape,
                                    torch_data_t dtype, torch_device_t device);
+
+/**
+ * Function to generate an empty Torch Tensor
+ * @param number of dimensions of the Tensor
+ * @param shape of the Tensor
+ * @param data type of the elements of the Tensor
+ * @param device used (cpu, CUDA, etc.)
+ */
 EXPORT_C torch_tensor_t torch_empty(int ndim, const int64_t* shape,
                                     torch_data_t dtype, torch_device_t device);
+
+/**
+ * Function to create a Torch Tensor from memory location given extra information
+ * @param pointer to the Tensor in memory
+ * @param number of dimensions of the Tensor
+ * @param shape of the Tensor
+ * @param data type of the elements of the Tensor
+ * @param device used (cpu, CUDA, etc.)
+ * @return Torch Tensor interpretation of the data pointed at
+ */
 EXPORT_C torch_tensor_t torch_from_blob(void* data, int ndim,
                                         const int64_t* shape,
                                         torch_data_t dtype,
                                         torch_device_t device);
+
+/**
+ * Function to print out a Torch Tensor
+ * @param Torch Tensor to print
+ */
 EXPORT_C void torch_tensor_print(const torch_tensor_t tensor);
+
+/**
+ * Function to delete a Torch Tensor to clean up
+ * @param Torch Tensor to delete
+ */
 EXPORT_C void torch_tensor_delete(torch_tensor_t tensor);
 
+
+// =====================================================================================
 // Module API
+// =====================================================================================
+/**
+ * Function to load in a Torch model from a TorchScript file and store in a Torch Module
+ * @param filename where TorchScript description of model is stored
+ * @return Torch Module loaded in from file
+ */
 EXPORT_C torch_jit_script_module_t torch_jit_load(const char* filename);
+
+/**
+ * Function to run the `forward` method of a Torch Module
+ * @param Torch Module containing the model
+ * @param vector of Torch Tensors as inputs to the model
+ * @param number of input Tensors in the input vector
+ * @param the output Tensor from running the model
+ */
 EXPORT_C void torch_jit_module_forward(const torch_jit_script_module_t module,
                                        const torch_tensor_t *inputs, const int nin,
                                        torch_tensor_t output);
+
+/**
+ * Function to delete a Torch Module to clean up
+ * @param Torch Module to delete
+ */
 EXPORT_C void torch_jit_module_delete(torch_jit_script_module_t module);
 
 #endif /* C_TORCH_H*/
