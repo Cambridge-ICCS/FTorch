8523b05601e81c458e4955ef383fc0809603d4bf
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-06-05 09:51:28 +0000

    Remove conda directory (#18)

--- fortran-pytorch-lib/conda-recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#cd fortran-pytorch-lib
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH=${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-
-make
-make install
