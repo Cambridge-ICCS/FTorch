2c490d9c5702b25591e74bd419f3b90ddd097505
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2023-09-20 18:05:06 +0100

    Add example image to python prediction

--- examples/2_ResNet18/resnet18.py
+++ examples/2_ResNet18/resnet18.py
@@ -1,8 +1,10 @@
 """Load and run pretrained ResNet-18 from TorchVision."""
 
+import numpy as np
+from PIL import Image
 import torch
-import torch.nn.functional as F
 import torchvision
+import urllib
 
 
 # Initialize everything
@@ -29,20 +31,80 @@ def initialize():
 
 def run_model(model):
     """
-    Run the pre-trained ResNet-18 with dummy input of ones.
+    Run the pre-trained ResNet-18 with an example image of a dog.
 
     Parameters
     ----------
     model : torch.nn.Module
     """
 
-    print("Running ResNet-18 model for ones...", end="")
-    dummy_input = torch.ones(1, 3, 224, 224)
-    output = model(dummy_input)
-    top5 = F.softmax(output, dim=1).topk(5).indices
+    print("Downloading image...", end="")
+    url, filename = (
+        "https://github.com/pytorch/hub/raw/e55b003/images/dog.jpg",
+        "dog.jpg",
+    )
+    urllib.request.urlretrieve(url, filename)
     print("done.")
 
-    print(f"Top 5 results:\n  {top5}")
+    # Transform image into the  form expected by the model
+    input_image = Image.open(filename)
+    preprocess = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize(256),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    print("Saving input batch...", end="")
+    tensor_shape = input_batch.shape
+    np.savetxt(
+        "image_tensor.txt",
+        input_batch.numpy().reshape(
+            (tensor_shape[1], tensor_shape[2] * tensor_shape[3])
+        ),
+    )
+
+    # Check saved correctly
+    read_array = np.zeros(tensor_shape)
+    with open("image_tensor.txt") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            data = line.split(" ")
+            read_array[0, i, :, :] = np.array(data).reshape(
+                (tensor_shape[2], tensor_shape[3])
+            )
+
+    assert np.array_equal(read_array, input_batch.numpy()) is True
+    print("done.")
+
+    print("Running ResNet-18 model for input...", end="")
+    with torch.no_grad():
+        output = model(input_batch)
+    print("done.")
+
+    #  Run a softmax to get probabilities
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    # Download ImageNet labels
+    url, filename = (
+        "https://raw.githubusercontent.com/pytorch/hub/e55b003/imagenet_classes.txt",
+        "imagenet_classes.txt",
+    )
+    data = urllib.request.urlopen(url)
+    for _ in data:
+        categories = [s.strip() for s in data]
+
+    # Show top categories per image
+    top5_prob, top5_catid = torch.topk(probabilities, 5)
+    print("\nTop 5 results:\n")
+    for i in range(top5_prob.size(0)):
+        print(categories[top5_catid[i]], top5_prob[i].item())
 
 
 if __name__ == "__main__":
