038457e1fbe5233cba1f897d1cc6fc1f785c51c8
aysha2016 <amunawwara@gmail.com>
2023-11-18 22:00:39 +0300

    Update resnet18.py
    Command-Line Arguments: Modify the script to accept command-line arguments, making it more versatile. Users can specify the input image file.
    
    python  resnet18.py  dog.jpeg --precision fp32 --model_type resnet18

--- examples/2_ResNet18/resnet18.py
+++ examples/2_ResNet18/resnet18.py
@@ -5,8 +5,6 @@ from PIL import Image
 import torch
 import torchvision
 import argparse
-import matplotlib.pyplot as plt
-import cv2
 
 # Initialize everything
 def initialize(precision: torch.dtype) -> torch.nn.Module:
@@ -37,23 +35,8 @@ def initialize(precision: torch.dtype) -> torch.nn.Module:
     model.eval()
 
     return model
-
-
-def run_model(model: torch.nn.Module, precision: type) -> None:
-    """
-    Run the pre-trained ResNet-18 with an example image of a dog.
-
-    Parameters
-    ----------
-    model: torch.nn.Module
-        Pretrained model to run.
-    precision: type
-        NumPy data type to save input tensor.
-    """
-    # Transform image into the form expected by the pre-trained model, using the mean
-    # and standard deviation from the ImageNet dataset
-    # See: https://pytorch.org/vision/0.8/models.html
-    image_filename = "data/dog.jpg"
+    
+def run_model(model: torch.nn.Module, precision: type, image_filename: str) -> None:
     input_image = Image.open(image_filename)
     preprocess = torchvision.transforms.Compose(
         [
@@ -65,15 +48,6 @@ def run_model(model: torch.nn.Module, precision: type) -> None:
             ),
         ]
     )
-      # Show the input image
-    plt.figure(figsize=(6, 6))
-    plt.imshow(input_image)
-    plt.title("Input Image")
-    plt.axis('off')
-    plt.show()
-
-
-    
     input_tensor = preprocess(input_image)
     input_batch = input_tensor.unsqueeze(0)
 
@@ -84,11 +58,11 @@ def run_model(model: torch.nn.Module, precision: type) -> None:
     )  # type: np.typing.NDArray
 
     # Save data as binary
-    np_input.tofile("data/image_tensor.dat")
+    np_input.tofile("image_tensor.dat")
 
     # Load saved data to check it was saved correctly
     np_data = np.fromfile(
-        "data/image_tensor.dat", dtype=precision
+        "image_tensor.dat", dtype=precision
     )  # type: np.typing.NDArray
 
     # Reshape to original tensor shape
@@ -103,64 +77,36 @@ def run_model(model: torch.nn.Module, precision: type) -> None:
         output = model(input_batch)
     print("done.")
 
-    print_top_results(output)
+    print(output)    
+    
+if __name__ == "__main__":  
 
-"""
-def print_top_results(output: torch.Tensor) -> None:
-    """Prints top 5 results
-
-    Parameters
-    ----------
-    output: torch.Tensor
-        Output from ResNet-18.
-    """
-    #  Run a softmax to get probabilities
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-
-    # Read ImageNet labels from text file
-    cats_filename = "data/categories.txt"
-    categories = np.genfromtxt(cats_filename, dtype=str, delimiter="\n")
-
-    # Show top categories per image
-    top5_prob, top5_catid = torch.topk(probabilities, 5)
-    print("\nTop 5 results:\n")
-    for i in range(top5_prob.size(0)):
-        cat_id = top5_catid[i]
-        print(
-            f"{categories[cat_id]} (id={cat_id}): probability = {top5_prob[i].item()}"
-        )
-"""
-
-if __name__ == "__main__":
- """   np_precision = np.float32
-
-    if np_precision == np.float32:
-        torch_precision = torch.float32
-    elif np_precision == np.float64:
-        torch_precision = torch.float64
-    else:
-        raise ValueError("`np_precision` must be of type `np.float32` or `np.float64`")
-
-    rn_model = initialize(torch_precision)
-    run_model(rn_model, np_precision)
-"""
-
-    # Create an argument parser
+ # Define command-line arguments
     parser = argparse.ArgumentParser(description="ResNet-18 Image Classification")
-    parser.add_argument("image_filename", help="Path to the input image file")
+    parser.add_argument("image_path", type=str, help="Path to the input image file")
     parser.add_argument(
         "--precision",
-        choices=["float32", "float64"],
-        default="float32",
-        help="Working precision (default: float32)",
+        choices=["fp32", "fp64"],
+        default="fp32",
+        help="Working precision (default: fp32)",
     )
-
+    parser.add_argument(
+        "--model_type",
+        choices=["resnet18"],
+        default="resnet18",
+        help="Model type (default: resnet18)",
+    )
     args = parser.parse_args()
 
-    # Map precision argument to torch data type
-    precision_map = {"float32": torch.float32, "float64": torch.float64}
-    np_precision = np.float32 if args.precision == "float32" else np.float64
-    torch_precision = precision_map[args.precision]
+    # Map command-line precision argument to NumPy and Torch data types
+    if args.precision == "fp32":
+        np_precision = np.float32
+        torch_precision = torch.float32
+    elif args.precision == "fp64":
+        np_precision = np.float64
+        torch_precision = torch.float64
+    else:
+        raise ValueError("Precision must be 'fp32' or 'fp64'")
 
     rn_model = initialize(torch_precision)
-    run_model(rn_model, np_precision, args.image_filename)
+    run_model(rn_model, np_precision, args.image_path)
