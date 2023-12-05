84b149beb942c04ba2cb3e9f33a10bbc28509d45
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2023-12-05 16:15:36 +0000

    Refactor setting eval

--- src/ctorch.cpp
+++ src/ctorch.cpp
@@ -43,6 +43,16 @@ constexpr auto get_device(torch_device_t device)
   }
 }
 
+void set_is_training(torch_jit_script_module_t module, const bool is_training)
+{
+  auto model = static_cast<torch::jit::script::Module*>(module);
+  if (is_training) {
+    model->train();
+  } else {
+    model->eval();
+  }
+}
+
 torch_tensor_t torch_zeros(int ndim, const int64_t* shape, torch_data_t dtype,
                            torch_device_t device, const bool requires_grad)
 {
@@ -171,11 +181,7 @@ torch_jit_script_module_t torch_jit_load(const char* filename,
     delete module;
     exit(EXIT_FAILURE);
   }
-  if (is_training) {
-    module->train();
-  } else {
-    module->eval();
-  }
+  set_is_training(module, is_training);
 
   return module;
 }
