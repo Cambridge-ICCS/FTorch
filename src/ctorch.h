aa714fe4fb8d6222659bda3755f86588f4f06029
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-04-04 00:29:04 +0100

    Add MacOS GPU device option

--- src/ctorch.h
+++ src/ctorch.h
@@ -28,7 +28,7 @@ typedef enum {
 } torch_data_t;
 
 // Device types
-typedef enum { torch_kCPU, torch_kCUDA } torch_device_t;
+typedef enum { torch_kCPU, torch_kCUDA, torch_kMPS } torch_device_t;
 
 // =====================================================================================
 // Tensor API
