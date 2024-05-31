dcf1a80b9a3bcbe6255776261b5bb6d6dcd531b0
Joe Wallwork <jw2423@cam.ac.uk>
2024-05-31 09:49:49 +0100

    Add BUILD_DIR in run script

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -3,6 +3,8 @@
 # Execute this shell script to run all of FTorch's integration tests.
 #
 # Assumes FTorch has been built with the `-DCMAKE_BUILD_TESTS=TRUE` option.
+# The `BUILD_DIR` variable in this script should be updated as appropriate for
+# your configuration.
 #
 # See `src/test/README.md` for more details on integration testing.
 # ---
@@ -11,8 +13,9 @@ set -eux
 
 CTEST_ARGS=$@
 EXAMPLES="1_SimpleNet 2_ResNet18"
+BUILD_DIR=src/build
 
-cd src/build/test/examples
+cd ${BUILD_DIR}/test/examples
 for EXAMPLE in ${EXAMPLES}
 do
   cd ${EXAMPLE}
