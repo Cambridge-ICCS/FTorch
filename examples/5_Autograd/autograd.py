f7bca7ef7d406bd458dc0c49f72567594abff4b5
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-21 09:59:30 +0100

    Apply Black

--- examples/5_Autograd/autograd.py
+++ examples/5_Autograd/autograd.py
@@ -8,10 +8,10 @@ import torch
 a = torch.tensor([2.0, 3.0], requires_grad=True)
 b = torch.tensor([6.0, 4.0], requires_grad=True)
 
-Q = 3 * a ** 3 - b ** 2
+Q = 3 * a**3 - b**2
 
 external_grad = torch.tensor([1.0, 1.0])
 Q.backward(gradient=external_grad)
 
-assert torch.allclose(9 * a ** 2, a.grad)
+assert torch.allclose(9 * a**2, a.grad)
 assert torch.allclose(-2 * b, b.grad)
