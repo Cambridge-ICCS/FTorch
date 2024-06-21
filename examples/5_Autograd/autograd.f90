6aa37680f8810bc8ac14ed0ed5deef4488c6135a
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-21 15:38:18 +0100

    Tidy overloading test - FIXME

--- examples/5_Autograd/autograd.f90
+++ examples/5_Autograd/autograd.f90
@@ -1,81 +1,50 @@
 program example
+
+  ! Import precision info from iso
+  use, intrinsic :: iso_fortran_env, only : sp => real32
+
+  ! Import our library for interfacing with PyTorch's Autograd module
   use ftorch
+
   implicit none
 
+  ! Set working precision for reals
+  integer, parameter :: wp = sp
+
+   ! Set up Fortran data structures
+  real(wp), dimension(2), target :: in_data1
+  real(wp), dimension(2), target :: in_data2
+  real(wp), dimension(2), target :: out_data
   integer :: tensor_layout(1) = [1]
+
+   ! Set up Torch data structures
   type(torch_tensor) :: a, b, Q
 
-  call torch_tensor_from_array(a, [2.0, 3.0], tensor_layout, torch_kCPU,       &
-                               device_index=0) ! FIXME: requires_grad=.true.
-  call torch_tensor_from_array(b, [6.0, 4.0], tensor_layout, torch_kCPU,       &
-                               device_index=0) ! FIXME: requires_grad=.true.
+   ! Initialise data
+  in_data1(:) = [2.0, 3.0]
+  in_data2(:) = [6.0, 4.0]
 
-  ! TODO: Q = 3 * a ** 3 - 2 * b
-  !   Requires overloading elementary operations
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  Q = a
-  print *, "Q = a ="
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = a + b ="
-  Q = a + b
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = a - b ="
-  Q = a - b
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = a * b ="
-  Q = a * b
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = 3 * a ="
-  Q = 3 * a
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  ! ---
-  print *, "Q = b * 2 ="
-  Q = b * 2
-  call torch_tensor_print(Q)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = a / b ="
-  Q = a / b
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
-  print *, "b ="
-  call torch_tensor_print(b)
-  ! ---
-  print *, "Q = a ** 2 ="
-  Q = a ** 2
-  call torch_tensor_print(Q)
-  print *, "a ="
-  call torch_tensor_print(a)
+  ! FIXME: requires_grad=.true.
+  call torch_tensor_from_array(a, in_data1, tensor_layout, torch_kCPU)
+  call torch_tensor_from_array(b, in_data2, tensor_layout, torch_kCPU)
+  call torch_tensor_from_array(Q, out_data, tensor_layout, torch_kCPU)
+
+  ! Check arithmetic operations work for torch_tensors
+  write (*,*) "a = ", in_data1(:)
+  write (*,*) "b = ", in_data2(:)
+  Q = 3 * a ** 3 - b ** 2
+  write (*,*) "Q = 3 * a ** 3 - b ** 2 =", out_data(:)
+
+  ! Check a and b are unchanged by the arithmetic operations
+  write (*,*) "a = ", in_data1(:)
+  write (*,*) "b = ", in_data2(:)
 
   ! TODO: Backward
   !   Requires API extension
+
+  ! Cleanup
+  call torch_tensor_delete(a)
+  call torch_tensor_delete(b)
+  call torch_tensor_delete(Q)
 
 end program example
--- examples/5_Autograd/autograd.py
+++ examples/5_Autograd/autograd.py
@@ -9,6 +9,7 @@ a = torch.tensor([2.0, 3.0], requires_grad=True)
 b = torch.tensor([6.0, 4.0], requires_grad=True)
 
 Q = 3 * a**3 - b**2
+print(Q)
 
 external_grad = torch.tensor([1.0, 1.0])
 Q.backward(gradient=external_grad)
