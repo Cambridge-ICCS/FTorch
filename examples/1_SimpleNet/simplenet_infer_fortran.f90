b5f53f2069371beb6c5b7ddaec2edb4ff8bc9dbd
Joe Wallwork <jw2423@cam.ac.uk>
2024-07-23 11:44:01 +0100

    Revert changes to example 1

--- examples/1_SimpleNet/simplenet_infer_fortran.f90
+++ examples/1_SimpleNet/simplenet_infer_fortran.f90
@@ -16,8 +16,7 @@ program inference
 
    ! Set up Fortran data structures
    real(wp), dimension(5), target :: in_data
-   real(wp), dimension(5), target :: dummy_data
-   real(wp), dimension(:), pointer :: out_data
+   real(wp), dimension(5), target :: out_data
    integer :: tensor_layout(1) = [1]
 
    ! Set up Torch data structures
@@ -36,26 +35,18 @@ program inference
    ! Initialise data
    in_data = [0.0, 1.0, 2.0, 3.0, 4.0]
 
-   ! Create Torch input tensor from the above array
+   ! Create Torch input/output tensors from the above arrays
    call torch_tensor_from_array(in_tensors(1), in_data, tensor_layout, torch_kCPU)
-
-   ! Create an empty Torch output tensor
-   ! TODO: Drop dummy_data and initialise out_tensors with torch_tensor_empty
-   call torch_tensor_from_array(out_tensors(1), dummy_data, tensor_layout, torch_kCPU)
+   call torch_tensor_from_array(out_tensors(1), out_data, tensor_layout, torch_kCPU)
 
    ! Load ML model
    call torch_model_load(model, args(1))
 
    ! Infer
    call torch_model_forward(model, in_tensors, out_tensors)
-
-   ! Extract the output as a Fortran array
-   allocate(out_data(5))
-   call torch_tensor_to_array(out_tensors(1), out_data)
    write (*,*) out_data(:)
 
    ! Cleanup
-   nullify(out_data)
    call torch_delete(model)
    call torch_delete(in_tensors)
    call torch_delete(out_tensors)
