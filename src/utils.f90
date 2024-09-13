47e95bffcc469e994a2d2e434718aabe11cc0ade
Joe Wallwork <jw2423@cam.ac.uk>
2024-09-13 15:59:11 +0100

    Rework assert function; implement 1d array version

--- src/utils.f90
+++ src/utils.f90
@@ -12,10 +12,10 @@ module utils
   contains
 
     !> Print the result of a test to the terminal
-    subroutine test_print(test_name, relative_error, test_pass)
+    subroutine test_print(test_name, message, test_pass)
 
       character(len=*), intent(in) :: test_name !! Name of the test being run
-      real, intent(in) :: relative_error !! Relative error used for assertion
+      character(len=*), intent(in) :: message !! Message to print
       logical, intent(in) :: test_pass !! Result of the assertion
 
       character(len=15) :: report
@@ -25,19 +25,21 @@ module utils
       else
         report = char(27)//'[31m'//'FAILED'//char(27)//'[0m'
       end if
-      write(*, '(A, " :: [", A, "] maximum relative error = ", E11.4)') report, trim(test_name), relative_error
+      write(*, '(A, " :: [", A, "] ", A)') report, trim(test_name), trim(message)
     end subroutine test_print
 
-   !> Asserts that two real values coincide to a given relative tolerance
-   function assert_real(got, expect, test_name, rtol, print_result) result(test_pass)
+    !> Asserts that two real values coincide to a given relative tolerance
+    function assert_real(got, expect, test_name, rtol, print_result) result(test_pass)
 
       character(len=*), intent(in) :: test_name !! Name of the test being run
       real, intent(in) :: got !! The value to be tested
       real, intent(in) :: expect !! The expected value
-      real, optional :: rtol !! Optional relative tolerance (defaults to 1e-5)
-      logical, optional :: print_result !! Optionally print test result to screen (defaults to .true.)
+      real, intent(in), optional :: rtol !! Optional relative tolerance (defaults to 1e-5)
+      logical, intent(in), optional :: print_result !! Optionally print test result to screen (defaults to .true.)
 
-      logical :: test_pass !! Did the assertion pass
+      logical :: test_pass !! Did the assertion pass?
+
+      character(len=80) :: message
 
       real :: relative_error
       real :: rtol_value
@@ -55,12 +57,59 @@ module utils
         print_result_value = print_result
       end if
 
-      test_pass = (abs(got - expect) <= rtol_value * expect)
+      test_pass = (abs(got - expect) <= rtol_value * abs(expect))
 
       if (print_result_value) then
-        call test_print(test_name, rtol_value, test_pass)
+        write(message,'("relative tolerance = ", E11.4)') rtol_value
+        call test_print(test_name, message, test_pass)
       end if
 
-   end function assert_real
+    end function assert_real
+
+    !> Asserts that two real-valued arrays coincide to a given relative tolerance
+    function assert_real_array_1d(got, expect, test_name, rtol, print_result) result(test_pass)
+
+      character(len=*), intent(in) :: test_name !! Name of the test being run
+      real, intent(in), dimension(:) :: got !! The value to be tested
+      real, intent(in), dimension(:) :: expect !! The expected value
+      real, intent(in), optional :: rtol !! Optional relative tolerance (defaults to 1e-5)
+      logical, intent(in), optional :: print_result !! Optionally print test result to screen (defaults to .true.)
+
+      logical :: test_pass !! Did the assertion pass?
+
+      character(len=80) :: message
+
+      real :: relative_error
+      real :: rtol_value
+      integer :: shape_error
+      logical :: print_result_value
+
+      if (.not. present(rtol)) then
+        rtol_value = 1.0e-5
+      else
+        rtol_value = rtol
+      end if
+
+      if (.not. present(print_result)) then
+        print_result_value = .true.
+      else
+        print_result_value = print_result
+      end if
+
+      ! Check the shapes of the arrays match
+      shape_error = maxval(abs(shape(got) - shape(expect)))
+      test_pass = (shape_error == 0)
+
+      if (test_pass) then
+        test_pass = all(abs(got - expect) <= rtol_value * abs(expect))
+        if (print_result_value) then
+          write(message,'("relative tolerance = ", E11.4)') rtol_value
+          call test_print(test_name, message, test_pass)
+        end if
+      else if (print_result_value) then
+        call test_print(test_name, "Arrays have mismatching shapes.", test_pass)
+      endif
+
+    end function assert_real_array_1d
 
 end module utils
