7cfeef7c9b13cf0cbe0d9b52fce8f17e97b2f68f
Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>
2024-09-13 15:34:29 +0100

    Implement `torch_tensor_to_array` (#161)
    * Implement torch_to_blob on C++ side
    
    * Implement Fortran interface
    
    * Use torch_tensor_to_array in example 1
    
    * Use correct data types; raise errors for unsupported cases
    
    * Revert changes to example 1
    
    * Add beginnings of autograd demo
    
    * Docs for example 5
    
    * More detail on uint8 and float16 not being supported
    
    * Add notes on float types
    
    * Handle allocation of pointer array
    
    * Comments on handling pointer array allocation

--- src/ctorch.cpp
+++ src/ctorch.cpp
@@ -7,7 +7,9 @@ constexpr auto get_dtype(torch_data_t dtype)
 {
   switch (dtype) {
   case torch_kUInt8:
-    return torch::kUInt8;
+    std::cerr << "[WARNING]: uint8 not supported in Fortran" << std::endl;
+    // See https://gcc.gnu.org/onlinedocs/gfortran/ISO_005fFORTRAN_005fENV.html
+    exit(EXIT_FAILURE);
   case torch_kInt8:
     return torch::kInt8;
   case torch_kInt16:
@@ -17,7 +19,9 @@ constexpr auto get_dtype(torch_data_t dtype)
   case torch_kInt64:
     return torch::kInt64;
   case torch_kFloat16:
-    return torch::kFloat16;
+    std::cerr << "[WARNING]: float16 not supported in Fortran" << std::endl;
+    // See https://gcc.gnu.org/onlinedocs/gfortran/ISO_005fFORTRAN_005fENV.html
+    exit(EXIT_FAILURE);
   case torch_kFloat32:
     return torch::kFloat32;
   case torch_kFloat64:
@@ -171,6 +175,45 @@ torch_tensor_t torch_from_blob(void* data, int ndim, const int64_t* shape,
     exit(EXIT_FAILURE);
   }
   return tensor;
+}
+
+void* torch_to_blob(const torch_tensor_t tensor, const torch_data_t dtype)
+{
+    auto t = reinterpret_cast<torch::Tensor* const>(tensor);
+    void* raw_ptr;
+    switch (dtype) {
+    case torch_kUInt8:
+      std::cerr << "[WARNING]: uint8 not supported" << std::endl;
+      exit(EXIT_FAILURE);
+    case torch_kInt8:
+      raw_ptr = (void*) t->data_ptr<int8_t>();
+      break;
+    case torch_kInt16:
+      raw_ptr = (void*) t->data_ptr<int16_t>();
+      break;
+    case torch_kInt32:
+      raw_ptr = (void*) t->data_ptr<int32_t>();
+      break;
+    case torch_kInt64:
+      raw_ptr = (void*) t->data_ptr<int64_t>();
+      break;
+    case torch_kFloat16:
+      std::cerr << "[WARNING]: float16 not supported" << std::endl;
+      // NOTE: std::float16_t is available but only with C++23
+      exit(EXIT_FAILURE);
+    case torch_kFloat32:
+      raw_ptr = (void*) t->data_ptr<float>();
+      // NOTE: std::float32_t is available but only with C++23
+      break;
+    case torch_kFloat64:
+      raw_ptr = (void*) t->data_ptr<double>();
+      // NOTE: std::float64_t is available but only with C++23
+      break;
+    default:
+      std::cerr << "[WARNING]: unknown data type" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    return raw_ptr;
 }
 
 void torch_tensor_print(const torch_tensor_t tensor)
