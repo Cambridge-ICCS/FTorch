5c23929d5fe8b992581fb974a051a36ef3243158
Dominic Orchard <dom.orchard@gmail.com>
2024-09-16 09:31:22 +0100

    generalising tensor.model to take a list of vectors, and trying the equivalent on the ftorch side

---
+++ examples/5_Batched/simplenet.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Module defining a simple PyTorch 'Net' for coupling to Fortran, using batching."""
+
+import torch
+from torch import nn
+
+
+class SimpleNetBatch(nn.Module):
+    """PyTorch module multiplying an input vector by 2."""
+
+    def __init__(
+        self,
+    ) -> None:
+        """
+        Initialize the SimpleNet model.
+
+        Consists of a single Linear layer with weights predefined to
+        multiply the input by 2.
+        """
+        super().__init__()
+        self._fwd_seq = nn.Sequential(
+            nn.Linear(5, 5, bias=False),
+        )
+        with torch.no_grad():
+            self._fwd_seq[0].weight = nn.Parameter(2.0 * torch.eye(5))
+
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
+        """
+        Pass ``batch`` through the model.
+
+        Parameters
+        ----------
+        batch : torch.Tensor
+            A mini-batch of input vectors of length 5.
+
+        Returns
+        -------
+        torch.Tensor
+            batch scaled by 2.
+
+        """
+        return self._fwd_seq(batch)
+
+
+if __name__ == "__main__":
+    model = SimpleNet()
+    model.eval()
+
+    # Batch together two vectors
+    x = [0.0, 1.0, 2.0, 3.0, 4.0]
+    y = [1.0, 2.0, 3.0, 4.0, 5.0]
+    # Run inference
+    with torch.no_grad():
+        print(model(torch.Tensor([x, y])))
---
+++ examples/5_Batched/simplenet_infer_python.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Load saved SimpleNet to TorchScript and run inference example."""
+
+import os
+import sys
+import torch
+
+
+def deploy(saved_model: str, device: str, batch_size: int = 1) -> torch.Tensor:
+    """
+    Load TorchScript SimpleNet and run inference with example Tensor.
+
+    Parameters
+    ----------
+    saved_model : str
+        location of SimpleNet model saved to Torchscript
+    device : str
+        Torch device to run model on, 'cpu' or 'cuda'
+    batch_size : int
+        batch size to run (default 1)
+
+    Returns
+    -------
+    output : torch.Tensor
+        result of running inference on model with example Tensor input
+    """
+    # Batch together two vectors
+    x = [0.0, 1.0, 2.0, 3.0, 4.0]
+    y = [1.0, 2.0, 3.0, 4.0, 5.0]
+    input_tensor = torch.tensor([x,y]).repeat(batch_size, 1)
+
+    if device == "cpu":
+        # Load saved TorchScript model
+        model = torch.jit.load(saved_model)
+        # Inference
+        output = model.forward(input_tensor)
+
+    elif device == "cuda":
+        # All previously saved modules, no matter their device, are first
+        # loaded onto CPU, and then are moved to the devices they were saved
+        # from, so we don't need to manually transfer the model to the GPU
+        model = torch.jit.load(saved_model)
+        input_tensor_gpu = input_tensor.to(torch.device("cuda"))
+        output_gpu = model.forward(input_tensor_gpu)
+        output = output_gpu.to(torch.device("cpu"))
+
+    else:
+        raise ValueError(f"Device '{device}' not recognised.")
+
+    return output
+
+
+if __name__ == "__main__":
+    filepath = os.path.dirname(__file__) if len(sys.argv) == 1 else sys.argv[1]
+    saved_model_file = os.path.join(filepath, "saved_simplenet_model_cpu.pt")
+
+    device_to_run = "cpu"
+
+    batch_size_to_run = 1
+
+    with torch.no_grad():
+        result = deploy(saved_model_file, device_to_run, batch_size_to_run)
+
+    print(result)
