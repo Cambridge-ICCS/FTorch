ac5ec917f02d7c7ad2f789bc703a973021cb8bcd
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-15 15:20:00 +0000

    Rename test helper script; update docs

--- run_test_suite.sh
+++ run_test_suite.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # ---
-# Execute this shell script to run all of FTorch's integration tests.
+# Execute this shell script to run FTorch's test suite. This includes both unit
+# tests and integration tests.
 #
 # Assumes FTorch has been built with the `-DCMAKE_BUILD_TESTS=TRUE` option.
 # The `BUILD_DIR` variable in this script should be updated as appropriate for
 # your configuration.
 #
-# See `src/test/README.md` for more details on integration testing.
+# See `src/test/README.md` for more details on the test suite.
 # ---
 
 set -eu
 
-EXAMPLES="
-  1_SimpleNet
-  2_ResNet18
-  4_MultiIO
-  6_Autograd
-"
 BUILD_DIR=src/build
 
+# Unit tests
+cd ${BUILD_DIR}/test/unit
+ctest ${CTEST_ARGS}
+cd -
+
+# Integration tests
+EXAMPLES="1_SimpleNet 2_ResNet18 4_MultiIO 6_Autograd"
 for EXAMPLE in ${EXAMPLES}; do
   pip -q install -r examples/"${EXAMPLE}"/requirements.txt
   cd "${BUILD_DIR}"/test/examples/"${EXAMPLE}"
