7a10a91d23b166d5104c9831819a214119594c97
tommelt <thomas.meltzer1@gmail.com>
2024-12-09 11:56:38 +0000

    docs: update comments and docs for running windows tests

--- run_integration_tests.bat
+++ run_integration_tests.bat
@@ -1,3 +1,13 @@
+rem ---
+rem Execute this shell script to run all of FTorch's integration tests.
+rem
+rem Assumes FTorch has been built with the `-DCMAKE_BUILD_TESTS=TRUE` option.
+rem The `BUILD_DIR` variable in this script should be updated as appropriate for
+rem your configuration.
+rem
+rem See `src/test/README.md` for more details on integration testing.
+rem ---
+
 for /d %%i in (1_SimpleNet 2_ResNet18 4_MultiIO) do (
 pushd build\test\examples\%%i
 rem run the tests
