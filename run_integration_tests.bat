6748c213ecdcf2a363da90b51f7a697e0a74adbf
tommelt <thomas.meltzer1@gmail.com>
2024-12-09 10:33:20 +0000

    test: make run_integration_tests.bat similar to linux test

--- run_integration_tests.bat
+++ run_integration_tests.bat
@@ -1,4 +1,9 @@
-pushd %1
+for /d %%i in (1_SimpleNet 2_ResNet18 4_MultiIO) do (
+pushd build\test\examples\%%i
+rem run the tests
 ctest
+rem The following line will propagate the error back to the cmd shell
+rem This is necessary for the CI to detect a failed test
 if %errorlevel% neq 0 exit /b %errorlevel%
 popd
+)
