4418b579223233984ea5bb0e74cb5b2859dd843e
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-11 15:36:20 +0000

    Simplify autograd.py setup

--- examples/6_Autograd/autograd.py
+++ examples/6_Autograd/autograd.py
@@ -1,29 +1,15 @@
-"""Autograd demo taken from https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html."""
+"""Based on https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html."""
 
 import torch
 
+# Construct input tensors with requires_grad=True
 a = torch.tensor([2.0, 3.0], requires_grad=True)
 b = torch.tensor([6.0, 4.0], requires_grad=True)
 
+# Compute some mathematical expression
 Q = 3 * (a**3 - b * b / 3)
-print(Q)
-expect = torch.tensor([-12.0, 65.0])
-if not torch.allclose(Q, expect):
-    result_error = f"Result:\n{Q}\ndoes not match expected value:\n{expect}"
-    raise ValueError(result_error)
 
-external_grad = torch.tensor([1.0, 1.0])
-Q.backward(gradient=external_grad)
-
-if not torch.allclose(9 * a**2, a.grad):
-    result_error = (
-        f"Calculated gradient a.grad:\n{a.grad}\n"
-        "does not match expected 9 * a**2\n{9 * a**2}"
-    )
-    raise ValueError(result_error)
-if not torch.allclose(-2 * b, b.grad):
-    result_error = (
-        f"Calculated gradient b.grad:\n{b.grad}\n"
-        "does not match expected -2 * b\n{-2 * b}"
-    )
-    raise ValueError(result_error)
+# Reverse mode
+Q.backward(gradient=torch.ones_like(Q))
+print(a.grad)
+print(b.grad)
