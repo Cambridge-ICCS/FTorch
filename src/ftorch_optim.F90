c73c3dcf46c9a7f0944366977ac91c5e2c22779f
Jack Atkinson <jwa34@cam.ac.uk>
2025-03-12 17:30:31 +0000

    Add boilerplate ftorch_optim.F90 module and C++ ready for optimisers.

---
+++ src/ftorch_optim.F90
@@ -0,0 +1,24 @@
+!| Optimisers module for FTorch.
+!
+!  * License
+!    FTorch is released under an MIT license.
+!    See the [LICENSE](https://github.com/Cambridge-ICCS/FTorch/blob/main/LICENSE)
+!    file for details.
+
+module ftorch_optim
+
+  use, intrinsic :: iso_c_binding, only: c_associated, c_null_ptr, c_ptr
+  use, intrinsic :: iso_fortran_env, only: int32
+
+  use ftorch, only: ftorch_int
+
+  implicit none
+
+  public
+
+  ! ============================================================================
+  ! ---
+  ! ============================================================================
+
+
+end module ftorch_optim
