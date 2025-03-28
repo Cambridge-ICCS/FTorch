e40921697c7c3cf7585fc5472bfb83a77090caae
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-28 12:02:42 +0000

    Hook up retain_graph arg

--- src/ctorch.h
+++ src/ctorch.h
@@ -262,9 +262,11 @@ EXPORT_C void torch_tensor_zero_grad(torch_tensor_t tensor);
  * Note that the Tensor must have the requires_grad attribute set to true.
  * @param Tensor to perform back-propagation on
  * @param Tensor with an external gradient to supply for the back-propagation
+ * @param whether the computational graph should be retained
  */
 EXPORT_C void torch_tensor_backward(const torch_tensor_t tensor,
-                                    const torch_tensor_t external_gradient);
+                                    const torch_tensor_t external_gradient,
+                                    const bool retain_graph);
 
 /**
  * Function to return the grad attribute of a Torch Tensor.
