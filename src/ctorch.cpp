3443e84490e3b2f9e9e9a2109b9c2e1db0cbbaa3
Jack Atkinson <jwa34@cam.ac.uk>
2025-04-23 12:01:51 +0100

    Add error handling to C++ optimizers code.

--- src/ctorch.cpp
+++ src/ctorch.cpp
@@ -543,33 +543,59 @@ void torch_tensor_get_gradient(const torch_tensor_t tensor, torch_tensor_t gradi
 // --- Torch optimisers API
 // =============================================================================
 
+// Check if an optimizer is valid
+void validate_optimizer(const torch_optim_t optim, const std::string &name) {
+  if (!optim) {
+    throw std::invalid_argument(name + " is null.");
+  }
+}
+
 // Function to create an SGD optimizer and return a pointer to it
+// TODO: Wrap additional Options beyond learning rate
 torch_optim_t torch_optim_SGD(const torch_tensor_t *parameters, const int npar,
                               const double learning_rate) {
-  // Cast the parameters pointer into Tensor objects
-  auto params = reinterpret_cast<torch::Tensor *const *>(parameters);
-  // Generate a vector of Tensors populated from parameters
-  std::vector<torch::Tensor> parameters_vec;
-  for (int i = 0; i < npar; ++i) {
-    parameters_vec.push_back(*(params[i]));
-  }
+  try {
+    // Cast the parameters pointer into Tensor objects
+    auto params = reinterpret_cast<torch::Tensor *const *>(parameters);
 
-  // Create the optimizer and cast to torch_optim_t to return
-  auto optimizer_SGD =
-      new torch::optim::SGD(parameters_vec, torch::optim::SGDOptions(learning_rate));
-  return static_cast<torch_optim_t>(optimizer_SGD);
+    // Generate a vector of Tensors populated from parameters
+    std::vector<torch::Tensor> parameters_vec;
+    for (int i = 0; i < npar; ++i) {
+      validate_tensor(params[i],
+                      "Parameter at index " + std::to_string(i) + "in torch_optim_SGD");
+      parameters_vec.push_back(*(params[i]));
+    }
+
+    // Create the optimizer and cast to torch_optim_t to return
+    auto optimizer_SGD =
+        new torch::optim::SGD(parameters_vec, torch::optim::SGDOptions(learning_rate));
+    return static_cast<torch_optim_t>(optimizer_SGD);
+  } catch (const std::exception &e) {
+    ctorch_error(std::string(e.what()) + " in torch_optim_SGD");
+  }
+  return nullptr; // Return null in case of error
 }
 
 // Function to zero gradients for an optimizer
 void torch_optim_zero_grad(torch_optim_t optim) {
-  auto optimizer = static_cast<torch::optim::Optimizer *>(optim);
-  optimizer->zero_grad();
+  try {
+    auto optimizer = static_cast<torch::optim::Optimizer *>(optim);
+    validate_optimizer(optimizer, "Optimizer in torch_optim_zero_grad");
+    optimizer->zero_grad();
+  } catch (const std::exception &e) {
+    ctorch_error(std::string(e.what()) + " in torch_optim_zero_grad");
+  }
 }
 
 // Function to step an optimizer
 void torch_optim_step(torch_optim_t optim) {
-  auto optimizer = static_cast<torch::optim::Optimizer *>(optim);
-  optimizer->step();
+  try {
+    auto optimizer = static_cast<torch::optim::Optimizer *>(optim);
+    validate_optimizer(optimizer, "Optimizer in torch_optim_step");
+    optimizer->step();
+  } catch (const std::exception &e) {
+    ctorch_error(std::string(e.what()) + " in torch_optim_step");
+  }
 }
 
 // Function to delete an optimizer
