83ddb2bf2dcc70d2db6bffcd4782337992f50ff9
tommelt <tdm39@cam.ac.uk>
2025-06-03 13:39:30 +0100

    feat: add HIP support to cmake using CUDA backend (#385)
    * feat: add HIP support to cmake using CUDA backend
    
    * Add `torch_kHIP` and `hip` device type for `pt2ts` (#386)
    
    * Add GPU_DEVICE=HIP alias to CUDA
    
    * Update pt2ts scripts
    
    * Keep device_type str
    
    * Introduce torch_kHIP
    
    * Avoid duplicate case
    
    * Define torch_kHIP in Fortran
    
    * Lint
    
    * chore: add HIP support for CMakeLists for test and example
    
    * Update test/unit/CMakeLists.txt
    
    Co-authored-by: Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>
    
    * chore: fix cmake-lint error
    
    * chore: fix cmake-lint error
    
    * Apply suggestions from code review
    
    Co-authored-by: Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>

--- src/ftorch.F90
+++ src/ftorch.F90
@@ -62,6 +62,7 @@ module ftorch
   enum, bind(c)
     enumerator :: torch_kCPU = GPU_DEVICE_NONE
     enumerator :: torch_kCUDA = GPU_DEVICE_CUDA
+    enumerator :: torch_kHIP = GPU_DEVICE_HIP
     enumerator :: torch_kXPU = GPU_DEVICE_XPU
     enumerator :: torch_kMPS = GPU_DEVICE_MPS
   end enum
