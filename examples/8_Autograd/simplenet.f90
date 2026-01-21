5caf2c13c56c3e4fec314e3c33acd8053838f568
Joe Wallwork <jw2423@cam.ac.uk>
2026-01-21 11:20:46 +0000

    Add SimpleNet autograd demos in Python and Fortran

---
+++ examples/8_Autograd/simplenet.f90
@@ -0,0 +1,74 @@
+program autograd_simplenet
+
+  ! Import precision info from iso
+  use, intrinsic :: iso_fortran_env, only : sp => real32
+
+  ! Import our library for interfacing with PyTorch's Autograd module
+  use ftorch, only: torch_kCPU, torch_delete, torch_model, torch_model_load, torch_model_forward,  &
+                    torch_tensor, torch_tensor_backward, torch_tensor_from_array, &
+                    torch_tensor_get_gradient
+
+  ! Import our tools module for testing utils
+  use ftorch_test_utils, only : assert_allclose
+
+  implicit none
+
+  ! Set working precision for reals
+  integer, parameter :: wp = sp
+
+  integer, parameter :: ndims = 1
+  integer, parameter :: n = 5
+  real(wp), dimension(n), target :: in_array, out_array, grad_array
+  real(wp), dimension(n) :: expected
+
+  ! Flag for testing
+  logical :: test_pass
+
+  ! Set up Torch data structures
+  type(torch_model) :: model
+  type(torch_tensor) :: in_tensors(1), out_tensors(1) , grad_tensors(1)
+
+  ! Initialise Torch Tensors from input arrays as in Python example
+  in_array(:) = [1.0_wp, 2.0_wp, 3.0_wp, 4.0_wp, 5.0_wp]
+  call torch_tensor_from_array(in_tensors(1), in_array, torch_kCPU, requires_grad=.true.)
+
+  ! Initialise Torch Tensors from array used for output
+  call torch_tensor_from_array(out_tensors(1), out_array, torch_kCPU)
+
+  ! Load the model and run inference
+  call torch_model_load(model, trim("simplenet.pt"), torch_kCPU)
+  call torch_model_forward(model, in_tensors, out_tensors)
+
+  ! Check the output takes expected values
+  write(*,*) "Output:", out_array(:)
+  expected(:) = [2.0_wp, 4.0_wp, 6.0_wp, 8.0_wp, 10.0_wp]
+  if (.not. assert_allclose(out_array, expected, test_name="autograd_simplenet_y")) then
+    write(*,*) "Error :: value of y does not match expected value"
+    stop 999
+  end if
+
+  ! Run the backpropagation operator
+  ! This will perform backpropogation on the tensors involved in generating y (i.e., x), setting the
+  ! `grad` property for both of them.
+  call torch_tensor_backward(out_tensors(1))
+
+  ! Create tensors based off output arrays for the gradients and then retrieve them
+  call torch_tensor_from_array(grad_tensors(1), grad_array, torch_kCPU)
+  call torch_tensor_get_gradient(grad_tensors(1), in_tensors(1))
+
+  ! Check the gradients take expected values
+  write(*,*) "Gradient:", grad_array(:)
+  expected(:) = 2.0
+  if (.not. assert_allclose(grad_array, expected, test_name="autograd_simplenet_dydx")) then
+    write(*,*) "Error :: value of dydx does not match expected value"
+    stop 999
+  end if
+
+  call torch_delete(model)
+  call torch_delete(in_tensors)
+  call torch_delete(out_tensors)
+  call torch_delete(grad_tensors)
+
+  write (*,*) "SimpleNet Autograd example ran successfully"
+
+end program autograd_simplenet
---
+++ examples/8_Autograd/simplenet.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+
+class SimpleNet(nn.Module):
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+        self._fwd_seq = nn.Sequential(
+            nn.Linear(5, 5, bias=False),
+        )
+        self._fwd_seq[0].weight = nn.Parameter(2.0 * torch.eye(5))
+
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
+        return self._fwd_seq(batch)
+
+
+model = SimpleNet().eval()
+scripted_model = torch.jit.script(model)
+scripted_model.save("simplenet.pt")
+
+x = torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0]], requires_grad=True)
+y = scripted_model(x)
+
+dydx = torch.autograd.grad(
+    outputs=y,
+    inputs=x,
+    grad_outputs=torch.ones_like(y),
+    retain_graph=True,
+    allow_unused=True,
+)[0]
+
+print(f"y = {y}, dy/dx = {dydx}")
+
+assert torch.allclose(dydx, 2.0 * torch.ones_like(y))
