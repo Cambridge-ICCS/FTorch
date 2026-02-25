a5c39fe037e2ed8f7afa2ce4da50b6c2582d6d3a
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2026-02-25 08:04:45 +0000

    Deploying to gh-pages from @ Cambridge-ICCS/FTorch@b289b134e8db95ce49ed98d2ed13bed082330cb8 🚀

--- src/ftorch.f90
+++ src/ftorch.f90
@@ -6,20 +6,45 @@
 !    file for details.
 module ftorch
   use ftorch_devices, only: torch_kCPU, torch_kCUDA, torch_kHIP, torch_kXPU, torch_kMPS
+
   use ftorch_types, only: torch_kInt8, torch_kInt16, torch_kInt32, torch_kInt64, &
                           torch_kFloat32, torch_kFloat64, ftorch_int
-  use ftorch_tensor, only: assignment(=), operator(+), operator(-), operator(*), operator(/), &
-                           operator(**), torch_tensor, torch_tensor_empty, torch_tensor_zeros, &
-                           torch_tensor_ones, torch_tensor_from_array, torch_tensor_from_blob, &
-                           torch_tensor_to, torch_tensor_sum, torch_tensor_mean, &
-                           torch_tensor_backward, torch_tensor_get_gradient, torch_tensor_delete, &
-                           torch_tensor_array_delete, torch_tensor_get_rank, &
-                           torch_tensor_get_shape, torch_tensor_get_stride, &
-                           torch_tensor_get_dtype, torch_tensor_get_device_type, &
-                           torch_tensor_get_device_index, torch_tensor_requires_grad, &
-                           torch_tensor_zero, torch_tensor_zero_grad, torch_tensor_print
-  use ftorch_model, only: torch_model, torch_model_load, torch_model_forward, torch_model_delete, &
-                          torch_model_print_parameters, torch_model_is_training
+
+  use ftorch_tensor, only: torch_tensor, &  !--------------- (type)
+                           torch_tensor_from_array, &  !---- (constructors)
+                           torch_tensor_from_blob, &
+                           torch_tensor_empty, &
+                           torch_tensor_zeros, &
+                           torch_tensor_ones, &
+                           torch_tensor_delete, &  !-------- (destructor)
+                           assignment(=), &  !-------------- (operators)
+                           operator(+), &
+                           operator(-), &
+                           operator(*), &
+                           operator(/), &
+                           operator(**), &
+                           torch_tensor_get_rank, &  !------ (interrogation)
+                           torch_tensor_get_shape, &
+                           torch_tensor_get_stride, &
+                           torch_tensor_get_dtype, &
+                           torch_tensor_get_device_type, &
+                           torch_tensor_get_device_index, &
+                           torch_tensor_requires_grad, &
+                           torch_tensor_print, &
+                           torch_tensor_zero, &  ! --------- (manipulation)
+                           torch_tensor_zero_grad, &
+                           torch_tensor_to, &  !------------ (other procedures)
+                           torch_tensor_sum, &
+                           torch_tensor_mean, &
+                           torch_tensor_get_gradient, &  !-- (autograd procedures)
+                           torch_tensor_backward
+
+  use ftorch_model, only: torch_model, &  !--------------------- (type)
+                          torch_model_load, &  !---------------- (constructor)
+                          torch_model_delete, &  !-------------- (destructor)
+                          torch_model_print_parameters, &  !---- (interrogation)
+                          torch_model_is_training, &
+                          torch_model_forward  !---------------- (procedures)
 
   implicit none
 
@@ -27,9 +52,8 @@ module ftorch
 
   !> Interface for deleting generic torch objects
   interface torch_delete
-    module procedure torch_model_delete
     module procedure torch_tensor_delete
-    module procedure torch_tensor_array_delete
+    module procedure torch_model_delete
   end interface
 
 end module ftorch
