92d6b04d0cda9027f3051382405c5b3f2da93b9e
Joe Wallwork <jw2423@cam.ac.uk>
2026-02-25 14:00:44 +0000

    Make pt2ts runnable

--- utils/pt2ts.py
+++ utils/pt2ts.py
@@ -1,20 +1,10 @@
-"""Load a PyTorch model and convert it to TorchScript."""
-# Throughout this script there are various `FTORCH-TODO` comments indicating where
-# the user needs to modify as appropriate for their model
+#!/bin/env python
+"""Convert a PyTorch model file to TorchScript format."""
 
-import os
-from typing import Optional
-
-# FTORCH-TODO
-# Add a module import with your model here:
-# This example assumes the model architecture is in an adjacent module `my_ml_model.py`
-import my_ml_model
 import torch
 
 
-def script_to_torchscript(
-    model: torch.nn.Module, filename: Optional[str] = "scripted_model.pt"
-) -> None:
+def script_to_torchscript(model: torch.nn.Module, filename: str) -> None:
     """
     Save PyTorch model to TorchScript using scripting.
 
@@ -25,7 +15,8 @@ def script_to_torchscript(
     filename : str
         name of file to save to
     """
-    # FIXME: torch.jit.optimize_for_inference() when PyTorch issue #81085 is resolved
+    # FIXME: Adopt torch.jit.optimize_for_inference() once
+    # https://github.com/pytorch/pytorch/issues/81085 is resolved
     scripted_model = torch.jit.script(model)
     # print(scripted_model.code)
     scripted_model.save(filename)
@@ -33,8 +24,8 @@ def script_to_torchscript(
 
 def trace_to_torchscript(
     model: torch.nn.Module,
-    dummy_input: torch.Tensor,
-    filename: Optional[str] = "traced_model.pt",
+    input_tensor: torch.Tensor,
+    filename: str,
 ) -> None:
     """
     Save PyTorch model to TorchScript using tracing.
@@ -43,13 +34,14 @@ def trace_to_torchscript(
     ----------
     model : torch.NN.Module
         a PyTorch model
-    dummy_input : torch.Tensor
+    input_tensor : torch.Tensor
         appropriate size Tensor to act as input to model
     filename : str
         name of file to save to
     """
-    # FIXME: torch.jit.optimize_for_inference() when PyTorch issue #81085 is resolved
-    traced_model = torch.jit.trace(model, dummy_input)
+    # FIXME: Adopt torch.jit.optimize_for_inference() once
+    # https://github.com/pytorch/pytorch/issues/81085 is resolved
+    traced_model = torch.jit.trace(model, input_tensor)
     # traced_model.save(filename)
     frozen_model = torch.jit.freeze(traced_model)
     ## print(frozen_model.graph)
@@ -57,148 +49,116 @@ def trace_to_torchscript(
     frozen_model.save(filename)
 
 
-def load_torchscript(filename: Optional[str] = "saved_model.pt") -> torch.nn.Module:
+def load_torchscript(filename: str) -> torch.nn.Module:
     """
-    Load a TorchScript from file.
+    Load a TorchScript model from file.
 
     Parameters
     ----------
     filename : str
         name of file containing TorchScript model
     """
-    model = torch.jit.load(filename)
-
-    return model
+    return torch.jit.load(filename)
 
 
 if __name__ == "__main__":
     import argparse
+    import os
 
+    # Parse user input
     parser = argparse.ArgumentParser(
+        prog="pt2ts.py",
+        description="Convert a PyTorch model file to TorchScript format.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "--device_type",
-        help="Device type to run the inference on",
+        "input_model_file",
+        help="Filename for the model saved in PyTorch format, including path",
         type=str,
-        choices=["cpu", "cuda", "hip", "xpu", "mps"],
-        default="cpu",
     )
     parser.add_argument(
-        "--filepath",
-        help="Path to the file containing the PyTorch model",
+        "--output_model_file",
+        help="Filename for the model to be saved in TorchScript format, including path",
         type=str,
-        default=os.path.dirname(__file__),
+    )
+    parser.add_argument(
+        "--trace",
+        help="Apply tracing rather than scripting",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--input_tensor_file",
+        help=(
+            "Filename for the tensor saved in PyTorch format, including path (only"
+            " required if running with --trace). The tensor is used to determine the"
+            " dimensionality of the inputs so its values are ignored."
+        ),
+        type=str,
     )
     parsed_args = parser.parse_args()
-    device_type = parsed_args.device_type
-    filepath = parsed_args.filepath
+    input_model_file = parsed_args.input_model_file
+    output_model_file = parsed_args.output_model_file
+    trace = parsed_args.trace
+    input_tensor_file = parsed_args.input_tensor_file
 
-    # =====================================================
-    # Load model and prepare for saving
-    # =====================================================
-
-    # FTORCH-TODO
-    # Load a pre-trained PyTorch model
-    # Insert code here to load your model as `trained_model`.
-    # This example assumes my_ml_model has a method `initialize` to load
-    # architecture, weights, and place in inference mode
-    trained_model = my_ml_model.initialize()
-
-    # Switch off specific layers/parts of the model that behave
-    # differently during training and inference.
-    # This may have been done by the user already, so just make sure here.
-    trained_model.eval()
-
-    # =====================================================
-    # Prepare dummy input and check model runs
-    # =====================================================
-
-    # FTORCH-TODO
-    # Generate a dummy input Tensor `dummy_input` to the model of appropriate size.
-    # This example assumes two inputs of size (512x40) and (512x1)
-    trained_model_dummy_input_1 = torch.ones((512, 40), dtype=torch.float64)
-    trained_model_dummy_input_2 = torch.ones((512, 1), dtype=torch.float64)
-
-    # Transfer the model and inputs to GPU device, if appropriate
-    if device_type != "cpu":
-        if device_type == "hip":
-            device = torch.device("cuda")  # NOTE: HIP is treated as CUDA in FTorch
-        else:
-            device = torch.device(device_type)
-        trained_model = trained_model.to(device)
-        trained_model.eval()
-        trained_model_dummy_input_1 = trained_model_dummy_input_1.to(device)
-        trained_model_dummy_input_2 = trained_model_dummy_input_2.to(device)
-
-    # FTORCH-TODO
-    # Run model for dummy inputs
-    # If something isn't working This will generate an error
-    trained_model_dummy_outputs = trained_model(
-        trained_model_dummy_input_1,
-        trained_model_dummy_input_2,
-    )
-
-    # =====================================================
-    # Save model
-    # =====================================================
-
-    # FTORCH-TODO
-    # Set the name of the file you want to save the torchscript model to:
-    saved_ts_filename = f"saved_model_{device_type}.pt"
-    # A filepath may also be provided. To do this, pass the filepath as an argument to
-    # this script when it is run from the command line, i.e. `./pt2ts.py path/to/model`.
-
-    # FTORCH-TODO
-    # Save the PyTorch model using either scripting (recommended if possible) or tracing
-    # -----------
-    # Scripting
-    # -----------
-    script_to_torchscript(trained_model, filename=saved_ts_filename)
-
-    # -----------
-    # Tracing
-    # -----------
-    # trace_to_torchscript(
-    #     trained_model, trained_model_dummy_input, filename=saved_ts_filename
-    # )
-
-    # =====================================================
-    # Check model saved OK
-    # =====================================================
-
-    # Load torchscript and run model as a test, scaling inputs as above
-    trained_model_dummy_input_1 = 2.0 * trained_model_dummy_input_1
-    trained_model_dummy_input_2 = 2.0 * trained_model_dummy_input_2
-    trained_model_testing_outputs = trained_model(
-        trained_model_dummy_input_1,
-        trained_model_dummy_input_2,
-    )
-    ts_model = load_torchscript(filename=saved_ts_filename)
-    ts_model_outputs = ts_model(
-        trained_model_dummy_input_1,
-        trained_model_dummy_input_2,
-    )
-
-    if not isinstance(ts_model_outputs, tuple):
-        ts_model_outputs = (ts_model_outputs,)
-    if not isinstance(trained_model_testing_outputs, tuple):
-        trained_model_testing_outputs = (trained_model_testing_outputs,)
-    for ts_output, output in zip(ts_model_outputs, trained_model_testing_outputs):
-        if torch.all(ts_output.eq(output)):
-            print("Saved TorchScript model working as expected in a basic test.")
-            print("Users should perform further validation as appropriate.")
-        else:
-            model_error = (
-                "Saved Torchscript model is not performing as expected.\n"
-                "Consider using scripting if you used tracing, or investigate further."
-            )
-            raise RuntimeError(model_error)
-
-    # Check that the model file is created
-    if not os.path.exists(os.path.join(filepath, saved_ts_filename)):
-        torchscript_file_error = (
-            f"Saved TorchScript file {os.path.join(filepath, saved_ts_filename)} "
-            "cannot be found."
+    # Process input model file name
+    input_model_root, input_model_ext = os.path.splitext(input_model_file)[1]
+    if ext != ".pt":
+        value_error = (
+            f"PyTorch input file '{input_model_file}' has extension {input_model_ext}"
+            " but .pt was expected."
         )
-        raise FileNotFoundError(torchscript_file_error)
+        raise ValueError(value_error)
+    if not os.path.exists(input_model_file):
+        input_file_error = f"PyTorch model file '{input_model_file}' cannot be found."
+        raise FileNotFoundError(input_file_error)
+
+    # Process output model file name
+    if output_model_file is None:
+        output_model_file = input_model_root + ".ts"
+    _, output_model_ext = os.path.splitext(output_model_file)[1]
+    if ext not in (".ts", ".pt"):
+        value_error = (
+            f"TorchScript output file name '{output_model_file}' has extension"
+            f" {output_model_ext} but .ts (recommended) or .pt (legacy) was expected."
+        )
+        raise ValueError(value_error)
+    if input_model_file == output_model_file:
+        value_error = (
+            f"Output TorchScript file name '{output_model_file}' coincides with input"
+            f" PyTorch file name '{input_model_file}' and would overwrite it."
+        )
+        raise ValueError(value_error)
+    if os.path.exists(output_model_file):
+        value_error = (
+            "A file already exists with output TorchScript file name"
+            f" '{output_model_file}' and would be overwritten."
+        )
+        raise ValueError(value_error)
+
+    # Load the input PyTorch model
+    model = torch.load(input_model_file)
+
+    # Apply scripting or tracing as requested, writing out to file
+    if trace:
+        # Process input tensor file name
+        _, input_tensor_ext = os.path.splitext(input_model_file)[1]
+        if input_tensor_ext != ".pt":
+            value_error = (
+                f"PyTorch input tensor file '{input_tensor_file}' has extension"
+                f" {input_tensor_ext} but .pt was expected."
+            )
+            raise ValueError(value_error)
+        if not os.path.exists(input_tensor_file):
+            input_file_error = (
+                f"PyTorch tensor file '{input_tensor_file}' cannot be found."
+            )
+            raise FileNotFoundError(input_file_error)
+
+        # Load the input tensor
+        input_tensor = torch.load(input_tensor_file)
+
+        trace_to_torchscript(model, input_tensor, output_model_file)
+    else:
+        script_to_torchscript(model, output_model_file)
