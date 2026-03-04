9a08442da109c392f8d8f8520e15a3fb5b8f7668
Joe Wallwork <jw2423@cam.ac.uk>
2026-03-04 16:30:08 +0000

    Enable testing in pt2ts

--- ftorch_utils/pt2ts.py
+++ ftorch_utils/pt2ts.py
@@ -9,6 +9,7 @@ import torch
 
 from ftorch_utils.torchscript import (
     load_pytorch,
+    load_torchscript,
     script_to_torchscript,
     trace_to_torchscript,
 )
@@ -47,6 +48,15 @@ def main_cli():
         type=str,
     )
     parser.add_argument(
+        "--input_tensor_file",
+        help=(
+            "Filename for the tensor saved in PyTorch format, including path (only"
+            " required if running with --trace or --test). The tensor is used to"
+            " determine the dimensionality of the inputs so its values are ignored."
+        ),
+        type=str,
+    )
+    parser.add_argument(
         "--trace",
         help=(
             "Apply tracing rather than scripting.\n\n"
@@ -55,14 +65,12 @@ def main_cli():
         action="store_true",
     )
     parser.add_argument(
-        "--input_tensor_file",
+        "--test",
         help=(
-            "Filename for the tensor saved in PyTorch format, including path (only"
-            " required if running with --trace). The tensor is used to determine the"
-            " dimensionality of the inputs so its values are ignored.\n\n"
-            "Implies --trace."
+            "Run basic testing to check everything is working as expected.\n\n"
+            "If used then --input_tensor_file must also be provided."
         ),
-        type=str,
+        action="store_true",
     )
     parsed_args = parser.parse_args()
     model_definition_file = parsed_args.model_definition_file
@@ -70,7 +78,14 @@ def main_cli():
     input_model_file = parsed_args.input_model_file
     output_model_file = parsed_args.output_model_file
     trace = parsed_args.trace
+    test = parsed_args.test
     input_tensor_file = parsed_args.input_tensor_file
+    if input_tensor_file is None and trace:
+        value_error = "An input tensor must be provided to use --trace."
+        raise ValueError(value_error)
+    if input_tensor_file is None and test:
+        value_error = "An input tensor must be provided to use --test."
+        raise ValueError(value_error)
 
     # Process input model file name
     input_model_root, input_model_ext = os.path.splitext(input_model_file)
@@ -110,8 +125,7 @@ def main_cli():
     # Load the input PyTorch model
     model = load_pytorch(model_definition_file, model_name, input_model_file)
 
-    # Apply scripting or tracing as requested, writing out to file
-    if trace:
+    if test or trace:
         # Process input tensor file name
         _, input_tensor_ext = os.path.splitext(input_tensor_file)
         if input_tensor_ext != ".pt":
@@ -129,9 +143,42 @@ def main_cli():
         # Load the input tensor
         input_tensor = torch.load(input_tensor_file)
 
+    if test:
+        # Propagate the input tensor through the model
+        # If something isn't working This will generate an error
+        pt_model_outputs = model(input_tensor)
+
+    # Apply scripting or tracing as requested, writing out to file
+    if trace:
         trace_to_torchscript(model, input_tensor, output_model_file)
     else:
         script_to_torchscript(model, output_model_file)
+    print(f"Saved model to TorchScript in '{output_model_file}'.")
+
+    if test:
+        # Check that the model file is created
+        if not os.path.exists(output_model_file):
+            torchscript_file_error = (
+                f"Saved TorchScript file '{output_model_file}' cannot be found."
+            )
+            raise FileNotFoundError(torchscript_file_error)
+
+        # Load the TorchScript model and propagate the same input tensor
+        ts_model = load_torchscript(output_model_file)
+        ts_model_outputs = ts_model(input_tensor)
+        if not isinstance(ts_model_outputs, tuple):
+            ts_model_outputs = (ts_model_outputs,)
+        if not isinstance(pt_model_outputs, tuple):
+            pt_model_outputs = (pt_model_outputs,)
+        for ts_output, pt_output in zip(ts_model_outputs, pt_model_outputs):
+            if not torch.all(ts_output.eq(pt_output)):
+                model_error = (
+                    "Saved Torchscript model is not performing as expected.\n"
+                    "Consider using scripting if you used tracing, or investigate further."
+                )
+                raise RuntimeError(model_error)
+        print("Saved TorchScript model working as expected in a basic test.")
+        print("Users should perform further validation as appropriate.")
 
 
 if __name__ == "__main__":
