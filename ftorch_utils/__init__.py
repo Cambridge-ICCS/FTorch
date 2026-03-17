2b10d5421787306064f7486b26e8072ea938000d
Joe Wallwork <jw2423@cam.ac.uk>
2026-03-17 15:05:33 +0000

    Ruff lint

--- ftorch_utils/__init__.py
+++ ftorch_utils/__init__.py
@@ -1,4 +1,9 @@
-from .torchscript import load_pytorch, load_torchscript, script_to_torchscript, trace_to_torchscript
+from .torchscript import (
+    load_pytorch,
+    load_torchscript,
+    script_to_torchscript,
+    trace_to_torchscript,
+)
 
 __all__ = [
     "load_pytorch",
