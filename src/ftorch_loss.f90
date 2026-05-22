f9321e15bd479501cd3f3af1745a169dd4ab1782
Joe Wallwork <jw2423@cam.ac.uk>
2026-05-22 11:47:32 +0100

    Implement torch_loss_mse in Fortran

--- src/ftorch_loss.f90
+++ src/ftorch_loss.f90
@@ -12,9 +12,33 @@ module ftorch_loss
 
   public
 
-! contains
+contains
 
-  ! TODO: Implement MSELoss
+  ! ============================================================================
+  ! --- Procedures for evaluating specific loss functions
+  ! ============================================================================
+
+  !> Evaluate MSELoss
+  ! TODO: Allow reductions
+  subroutine torch_loss_mse(input_tensor, target_tensor, loss_tensor)
+    use, intrinsic :: iso_c_binding, only : c_ptr
+    type(torch_tensor), intent(in) :: input_tensor  !! Input tensor to evaluate loss at
+    type(torch_tensor), intent(in) :: target_tensor  !! Target tensor to evaluate loss against
+    type(torch_tensor), intent(inout) :: loss_tensor  !! Tensor to hold the loss value
+
+    interface
+      subroutine torch_loss_mse_c(input_tensor_c, target_tensor_c, loss_tensor_c) &
+          bind(c, name = 'torch_loss_mse')
+        use, intrinsic :: iso_c_binding, only : c_ptr
+        implicit none
+        type(c_ptr), value, intent(in) :: input_tensor_c
+        type(c_ptr), value, intent(in) :: target_tensor_c
+        type(c_ptr), value, intent(in) :: loss_tensor_c
+      end subroutine torch_loss_mse_c
+    end interface
+
+    call torch_loss_mse_c(input_tensor%p, target_tensor%p, loss_tensor%p)
+  end subroutine torch_loss_mse
 
   ! TODO: Implement CrossEntropyLoss
 
