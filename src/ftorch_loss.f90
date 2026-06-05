c7a466296401e7d0ab3a130d3d8afd8d4b05f157
Joe Wallwork <jw2423@cam.ac.uk>
2026-06-05 11:42:59 +0100

    Implement cross entropy loss

--- src/ftorch_loss.f90
+++ src/ftorch_loss.f90
@@ -55,4 +55,42 @@ contains
     call torch_loss_mse_c(loss_tensor%p, input_tensor%p, target_tensor%p, reduction_type_value)
   end subroutine torch_loss_mse
 
+  !> Evaluate CrossEntropyLoss
+  ! TODO: Support other optional args
+  subroutine torch_loss_cross_entropy(loss_tensor, input_tensor, target_tensor, reduction_type)
+    use, intrinsic :: iso_c_binding, only : c_associated, c_int
+    type(torch_tensor), intent(inout) :: loss_tensor  !! Tensor to hold the loss value
+    type(torch_tensor), intent(in) :: input_tensor  !! Input tensor to evaluate loss at
+    type(torch_tensor), intent(in) :: target_tensor  !! Target tensor to evaluate loss against
+    integer, optional, intent(in) :: reduction_type  !! Reduction type to use (default: torch_kMean)
+
+    integer(c_int) :: reduction_type_value
+
+    interface
+      subroutine torch_loss_cross_entropy_c(loss_tensor_c, input_tensor_c, target_tensor_c, &
+          reduction_type_c) bind(c, name = 'torch_loss_cross_entropy')
+        use, intrinsic :: iso_c_binding, only : c_ptr, c_int
+        implicit none
+        type(c_ptr), value, intent(in) :: loss_tensor_c
+        type(c_ptr), value, intent(in) :: input_tensor_c
+        type(c_ptr), value, intent(in) :: target_tensor_c
+        integer(c_int), value, intent(in) :: reduction_type_c
+      end subroutine torch_loss_cross_entropy_c
+    end interface
+
+    ! Process optional arguments
+    if (.not. present(reduction_type)) then
+      reduction_type_value = torch_kMean
+    else
+      reduction_type_value = reduction_type
+    end if
+
+    if (.not. c_associated(loss_tensor%p)) then
+      write(*,*) "Error :: loss tensor has not been constructed"
+      stop 1
+    end if
+    call torch_loss_cross_entropy_c(loss_tensor%p, input_tensor%p, target_tensor%p, &
+                                    reduction_type_value)
+  end subroutine torch_loss_cross_entropy
+
 end module ftorch_loss
